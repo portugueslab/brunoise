b380ac5e104eadee525bb4753f9b51eb305573d1
diegoasua <baxua@hotmail.es>
2020-02-05 20:38:23 +0100

    Added shutter control function

--- shutter_control.py
+++ shutter_control.py
@@ -1,13 +1,10 @@
 import nidaqmx
 from nidaqmx.constants import LineGrouping
 
-
 with nidaqmx.Task() as shutter_task:
-
-    #create a channel - units: Volts
-    shutter_task.do_channels.add_do_chan("Dev1/port0/line1", line_grouping=LineGrouping.CHAN_PER_LINE)
-    #define the task
-    shutter_task.write(5, auto_start=True)
-    #start the task
-    shutter_task.start()
-    shutter_task.wait_until_done()
+    # create a digital output channel
+    shutter_task.do_channels.add_do_chan("Dev1/port0/line1",line_grouping=LineGrouping.CHAN_PER_LINE)
+    # Change the status of the shutter
+    shutter_task.write(True, auto_start=True)
+    # Reset shutter
+    shutter_task.write(False, auto_start=True)
