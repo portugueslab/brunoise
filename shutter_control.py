c579e9a6c710d313d707cafcd61cee6c67fa8ec7
Vilim <vilim@neuro.mpg.de>
2020-02-06 14:54:04 +0100

    2p scanning working in the notebook

--- shutter_control.py
+++ shutter_control.py
@@ -1,10 +1,9 @@
 import nidaqmx
+from nidaqmx.constants import LineGrouping
 
 with nidaqmx.Task() as shutter_task:
-
-    #create a channel - units: Volts
-    shutter_task.ao_channels.add_ao_voltage_chan("Dev1/port0/line2", min_val=-5, max_val=5)
-    #define the task
-    shutter_task.write(5, auto_start=True)
-    #start the task
-    shutter_task.start()
+    # create a digital output channel
+    shutter_task.do_channels.add_do_chan("Dev1/port0/line1",line_grouping=LineGrouping.CHAN_PER_LINE)
+    # define the task\
+    shutter_task.write(True, auto_start=True)
+    shutter_task.write(False, auto_start=True)
