3be2f2c413e97f3d19f69e2461c29a3efcdd9644
Vilim <vilim@neuro.mpg.de>
2020-02-07 21:05:07 +0100

    Basic scaffolding for GUI with saving

--- twop/gui.py
+++ twop/gui.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
 from scanning import Scanner
+from streaming_save import StackSaver
 
 import pyqtgraph as pg
 import numpy as np
@@ -8,13 +9,19 @@ import numpy as np
 import qdarkstyle
 
 from queue import Empty
-import lightparam
+from lightparam import Param, Parametrized
 
+class ExperimentSettings(Parametrized):
+    def __init__(self):
+        self.n_frames = Param(10, (1, 10000))
+        self.n_planes = Param(1, (1, 500))
 
 class TwopViewer(QWidget):
     def __init__(self):
         super().__init__()
         self.scanner = Scanner()
+        self.save_queue = ArrayQueue()
+        self.saver = StackSaver()
         self.setLayout(QVBoxLayout())
         self.image_viewer = pg.ImageView()
         self.layout().addWidget(self.image_viewer)
@@ -27,6 +34,8 @@ class TwopViewer(QWidget):
         self.timer.start()
         self.scanner.start()
         self.first_image = True
+        
+        self.saving = False
 
     def update(self):
         try:
@@ -36,8 +45,13 @@ class TwopViewer(QWidget):
              autoRange=self.first_image,
              autoHistogramRange=self.first_image)
             self.first_image = False
+            if self.saving:
+                self.save_queue.put(current_image)
         except Empty:
             pass
+
+    def start_recording(self):
+        pass
 
     def stop(self):
         self.scanner.stop_event.set()
