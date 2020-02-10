eb349966cdfd1b6ed6c59b16694bcaba82f12562
diegoasua <baxua@hotmail.es>
2020-02-10 17:32:32 +0100

    Added shutter control function

---
+++ power_control.py
@@ -0,0 +1,71 @@
+import pyvisa
+from math import acos
+
+
+class LaserPowerControl:
+
+    def __init__(self, port, parity=pyvisa.constants.Parity.none, encoding="ascii"):
+        self.parity = parity
+        self.encoding = encoding
+        self.port = port
+        self.position = None
+        self.rm = pyvisa.ResourceManager
+        self.rotatory_stage = self.rm.open_resource(
+            port,
+            parity=parity,
+            encoding=encoding,
+            timeout=10,
+            resource_name='laser_power_control'
+        )
+        self.execute_home_search()
+        self.get_upper_bound()
+        self.get_lower_bound()
+        self.update_position()
+
+    def update_position(self):
+        device = '1'
+        input_m = device + 'TP'
+        output = self.rotatory_stage.query(input_m)
+        self.position = output
+
+    def execute_home_search(self):
+        device = '1'
+        input_m = device + 'OR'
+        self.rotatory_stage.query(input_m)
+
+    def get_upper_bound(self):
+        device = '1'
+        upper_bound = ''
+        input_m = device + 'SR' + upper_bound
+        self.rotatory_stage.query(input_m)
+
+    def get_lower_bound(self):
+        device = '1'
+        lower_bound = ''
+        input_m = device + 'SL' + lower_bound
+        self.rotatory_stage.query(input_m)
+
+    def move_abs(self, target_power_percent):
+        target_position = self.unit_transformer(target_power_percent)
+        device = '1'
+        input_m = device + 'SL' + str(target_position)
+        self.rotatory_stage.query(input_m)
+
+    def terminate_connection(self):
+        self.rm.close()
+
+    @staticmethod
+    def unit_transformer(
+            target_power_percent,
+            max_est_power=1088.318,
+            min_est_power=1.753,
+            amplitude=546.077,
+            vertical_shift=0.997,
+            frequency=0.07,
+            phase_shift=53.097
+    ):
+        # minimum and maximum power values are estimated based on model
+        # this function maps power percentage to degrees, parameters were phenomenologically determined
+        power_units = (max_est_power - min_est_power) * (target_power_percent / 100) - min_est_power
+        target_units: float = phase_shift + acos(power_units / amplitude - vertical_shift) / frequency
+        return target_units
