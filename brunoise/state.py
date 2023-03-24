f19b7d875ebc78a8b91059ce092cc38ad688df88
Nathan van Beelen <nathan@vanbeelen.org>
2023-03-24 17:05:14 +0100

    Make the frame duration more exact

--- brunoise/state.py
+++ brunoise/state.py
@@ -73,6 +73,9 @@ def convert_params(st: ScanningSettings) -> ScanningParameters:
     # Loosens the restraint by 2 * the turn value, as the first and last line require one turn less.
     n_total += 2 * st.n_turn
 
+    # Tightens the restraint by 1ms -> allows for less noise in frame duration further down the line.
+    n_total -= sample_rate / 1000.
+
     # Solving for the biggest image surface is basically a constraint problem of the form:
     # ax**2 + bx + c = 0, where a is the aspect ratio (can be seen as y * x where y = a * x), b is the two turns,
     # and c is the total number of available positions (given the desired frequency and sampling rate).
@@ -105,6 +108,7 @@ def convert_params(st: ScanningSettings) -> ScanningParameters:
         sample_rate_out=sample_rate,
         shutter=st.shutter,
         mystery_offset=mystery_offset,
+        framerate=st.framerate
     )
     return sp
 
