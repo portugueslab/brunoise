a5ce856f91226a6d37deada30241f6300053d7a6
Nathan van Beelen <nathan@vanbeelen.org>
2023-03-29 17:07:55 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="bruoise",
-    version="0.1.0",
+    version="0.1.1",
     author="Vilim Stih, Emanuele Paoli, Diego Asua, You Kure Wu, Nathan van Beelen @ Portugueslab",
     author_email="vilim@neuro.mpg.de",
     packages=find_packages(),
